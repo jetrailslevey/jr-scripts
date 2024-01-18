ada04365321f2a07387e28e390696ddc5a830aeb
David Levey <david@levey.me>
2024-01-18 09:48:08 -0700

    initial checks added

--- jr-server-eval.sh
+++ jr-server-eval.sh
@@ -1,1 +1,49 @@
 #!/bin/sh
+
+print_divider(){
+    echo "============================";
+}
+print_newline(){
+    echo "\n";
+}
+
+echo "OS Details";
+print_divider
+    uname -a
+print_divider
+print_newline
+
+echo "CPU Details";
+print_divider
+    lscpu
+    top -n 1 -b -c
+print_divider
+print_newline
+
+echo "Memory Details";
+print_divider
+    free -h
+print_divider
+print_newline
+
+echo "Disk Details";
+print_divider
+    lsblk -a
+    df -ha
+    iostat -dxm 1 3
+print_divider
+print_newline
+
+echo "Web Server Details";
+print_divider
+    /usr/sbin/nginx -T | egrep " server_name | root " |uniq
+    php -v
+    php -m
+print_divider
+print_newline
+
+echo "MySQL Details";
+print_divider
+    mysql -V
+    mysql -e 'SHOW VARABILES;'
+print_divider
