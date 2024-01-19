8b2b4e3fd719dcd7ffd0d4739bed1b0fdc251700
David Levey <david@levey.me>
2024-01-19 08:43:49 -0700

    added comments, improved webserver details, fixed mysql query

--- jr-server-eval.sh
+++ jr-server-eval.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+#
+# JetRails Server Evaluation Script
+#
+
 print_divider(){
     echo "============================";
 }
@@ -7,25 +11,32 @@ print_newline(){
     printf "\n";
 }
 
+## Begin OS Details
 echo "OS Details";
 print_divider
     uname -a
 print_divider
 print_newline
+## End OS Details
 
+## Begin CPU Details
 echo "CPU Details";
 print_divider
     lscpu
     top -n 1 -b -c
 print_divider
 print_newline
+## End CPU Details
 
+## Begin Memory Details
 echo "Memory Details";
 print_divider
     free -h
 print_divider
 print_newline
+## End Memory Details
 
+## Begin Disk Details
 echo "Disk Details";
 print_divider
     lsblk -a
@@ -33,17 +44,36 @@ print_divider
     iostat -dxm 1 3
 print_divider
 print_newline
+## End Disk Details
 
+## Begin Web Server Details
 echo "Web Server Details";
 print_divider
+
+webserver="";
+if [ ! -z "$(ps auxf |grep -E -i nginx |grep -v grep)" ]; then 
+    echo "Web Server: NGINX"; webserver=nginx; 
+fi
+if [ ! -z "$(ps auxf |grep -E -i httpd\|apache |grep -v grep)" ]; then 
+    echo "Web Server: Apache"; webserver=apache; 
+fi
+if [ -z "$webserver" ]; then 
+echo "Unknown Webserver"; 
+fi
+
+if [ $webserver=nginx ]; then 
     /usr/sbin/nginx -T | grep -E " server_name | root " | uniq
+fi
     php -v
     php -m
 print_divider
 print_newline
+## End Web Server Details
 
+## Begin MySQL Details
 echo "MySQL Details";
 print_divider
     mysql -V
-    mysql -e 'SHOW VARABILES;'
+    mysql -e 'SHOW VARIABLES;'
 print_divider
+## End MySQL Details
